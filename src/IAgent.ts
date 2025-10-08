4177063dae5286b1210a897072385de930930dbd
Troy Goode <troygoode@gmail.com>
2025-10-08 14:39:19 -0700

    flesh out IAgent

--- src/IAgent.ts
+++ src/IAgent.ts
@@ -1,4 +1,31 @@
+interface IStat {
+  score?: number;
+  distinguishingFeature?: string;
+}
+
+interface IBond {
+  description?: string;
+  strength?: number;
+}
+
+interface IWeapon {
+  description?: string;
+  skill?: string;
+  range?: string;
+  damage?: string;
+  ap?: string;
+  lethality?: string;
+  killRadius?: string;
+  ammo?: string;
+}
+
+interface ISpecialTraining {
+  description?: string;
+  skillOrStat?: string;
+}
+
 interface IAgent {
+  // personal
   name?: string;
   profession?: string;
   employer?: string;
@@ -7,6 +34,40 @@ interface IAgent {
   sexOther?: string;
   age?: string;
   education?: string;
+
+  // stats
+  strength?: IStat;
+  dexterity?: IStat;
+  constitution?: IStat;
+  intelligence?: IStat;
+  power?: IStat;
+  charisma?: IStat;
+  hp?: number;
+  wp?: number;
+  san?: number;
+  bp?: number;
+  physicalDescription?: string;
+
+  // pyschological
+  bonds?: IBond[];
+  motivationsAndDisorders?: string;
+  violenceAdaptation?: number;
+  helplessnessAdaptation?: number;
+
+  // skills
+
+  // injuries
+  wounds?: string;
+  firstAidAttempted?: boolean;
+
+  // equipment
+  gear?: string;
+  weapons?: IWeapon[];
+
+  // remarks
+  notes?: string;
+  developments?: string;
+  specialTraining?: ISpecialTraining[];
 }
 
 export { type IAgent };
