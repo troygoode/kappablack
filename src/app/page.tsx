2b72164ea2ff33819c46132b6315a92025b7e9ce
Troy Goode <troygoode@gmail.com>
2025-10-16 11:23:44 -0700

    basic auth.js implementation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
+import { SessionProvider } from "next-auth/react";
+
 import AgentPage from "@/components/agent/page";
 import { AgentStoreProvider } from "@/components/agent/stores/agent";
+import AuthButton from "@/components/auth/button";
 
 export default function Page() {
   return (
-    <AgentStoreProvider>
-      <AgentPage />
-    </AgentStoreProvider>
+    <SessionProvider>
+      <AgentStoreProvider>
+        <AuthButton />
+        <AgentPage />
+      </AgentStoreProvider>
+    </SessionProvider>
   );
 }
---
+++ src/components/auth/button.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import SignIn from "./signin";
+import { SignOut } from "./signout";
+
+export default function AuthButton() {
+  const { data: session } = useSession();
+
+  console.log("session", session);
+
+  return session?.user ? <SignOut /> : <SignIn />;
+}
