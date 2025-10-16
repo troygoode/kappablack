5f22e1f76bd24d4fa4d2c9ec6fa2970e41948f46
Troy Goode <troygoode@gmail.com>
2025-10-16 10:20:50 -0700

    bug fix

--- src/components/agent/equipment/equipment.tsx
+++ src/components/agent/equipment/equipment.tsx
@@ -63,7 +63,6 @@ export default function Equipment() {
                 <StunWeaponsCards
                   loading={!isLoaded}
                   weapons={stunWeapons}
-                  add={addWeapon}
                   onChange={updateStunWeapon}
                   remove={removeStunWeapon}
                 />
