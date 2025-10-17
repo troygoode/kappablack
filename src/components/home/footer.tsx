5b0a00af04db635fc3effa82e2644ed94739b34a
Troy Goode <troygoode@gmail.com>
2025-10-17 11:47:59 -0700

    ko-fi link

--- src/components/home/footer.tsx
+++ src/components/home/footer.tsx
@@ -3,15 +3,13 @@
 import Link from "next/link";
 import { GithubIcon } from "../ui/icons/lucide-github";
 import { Button } from "../ui/button";
+import { CoffeeIcon } from "../ui/icons/lucide-coffee";
 
 export default function Footer() {
   return (
     <ul className="w-full grid grid-cols-4 [&_a]:hover:underline [&_li]:flex [&_li]:items-center [&_li]:justify-center">
       <li>
         <Link href="/terms">Terms</Link>
-      </li>
-      <li>
-        <Link href="/cookies">Cookies </Link>
       </li>
       <li>
         <Link href="/privacy">Privacy</Link>
@@ -29,6 +27,22 @@ export default function Footer() {
           >
             <GithubIcon size={12} />
             <span>Github</span>
+          </Link>
+        </Button>
+      </li>
+      <li>
+        <Button
+          asChild
+          variant="link"
+          className="text-muted-foreground text-xs"
+        >
+          <Link
+            href="https://ko-fi.com/troygoode"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <CoffeeIcon size={12} />
+            <span>Tip Jar</span>
           </Link>
         </Button>
       </li>
---
+++ src/components/ui/icons/lucide-coffee.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+
+export function CoffeeIcon({
+  size = 24,
+  color = "currentColor",
+  strokeWidth = 2,
+  className,
+  ...props
+}: React.SVGProps<SVGSVGElement> & {
+  size?: number;
+  color?: string;
+  strokeWidth?: number;
+}) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke={color}
+      strokeWidth={strokeWidth}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={className}
+      {...props}
+    >
+      <path d="M10 2v2m4-2v2m2 4a1 1 0 0 1 1 1v8a4 4 0 0 1-4 4H7a4 4 0 0 1-4-4V9a1 1 0 0 1 1-1h14a4 4 0 1 1 0 8h-1M6 2v2"/>
+    </svg>
+  );
+}
